c7de4dec49f07e60629ff43cdeb2a02e10678ec3
Alysson <alyssuuuuu@outlook.com>
2025-03-19 00:46:55 -0300

    criação screen

---
+++ src/core/screen.py
@@ -0,0 +1,39 @@
+import abc
+import string
+from turtle import _Screen
+
+from errors.screens.duplicate_screen_id import DuplicateScreenId
+
+
+class Screen(abc.ABC):
+    
+    __id:string
+
+    @abc.abstractmethod
+    def __init__(self, identifier:string=None):
+
+        # verifica se tela com id ja existe
+        if not hasattr(Screen, '__list') or Screen.__list is not list:
+            Screen.__list = []
+        
+        if Screen.getScreenById(identifier) != None:
+            raise DuplicateScreenId()
+        
+        # inicializa screen
+        super().__init__()
+        self.__id = identifier
+        
+        Screen.__list.append(self)
+        
+        
+        
+    def id(self) -> string:
+        return self.__id
+    
+    
+    
+    @staticmethod
+    def getScreenById(id:string) -> _Screen:
+        for screen in Screen.__list:
+            if screen.id == id:
+                return screen
---
+++ src/errors/screens/duplicate_screen_id.py
@@ -0,0 +1,2 @@
+class DuplicateScreenId(Exception):
+    pass
