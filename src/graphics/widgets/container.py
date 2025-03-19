ab33443d402653106f84f1b3e006f9c97a67b2a1
Alysson <alyssuuuuu@outlook.com>
2025-03-19 19:47:59 -0300

    container

---
+++ src/graphics/widgets/container.py
@@ -0,0 +1,7 @@
+from graphics.widgets.widget import Widget
+
+
+class Container(Widget):
+
+    def __init__(self, x=0, y=0, width=0, height=0, color = ...):
+        super().__init__(x, y, width, height, color)
