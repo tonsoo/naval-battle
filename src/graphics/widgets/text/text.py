46ad64b953e2c51c08cab1c961ea1f29e823bc1f
Alysson <alyssuuuuu@outlook.com>
2025-04-21 21:14:43 -0300

    text

---
+++ src/graphics/widgets/text/text.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.freetype import Font
+from graphics.widgets.widget import Widget
+
+
+class Text(Widget):
+    _text:str = ''
+    _font:Font = None
+    _color:pygame.Color = (0,0,0)
+
+    def __init__(self, text:str='', x=0, y=0, color:pygame.Color = (0,0,0)):
+        super().__init__(x, y, 0, 0)
+
+        self._text = text
+
+    def font(self, font:Font):
+        self._font = font
+        return self
+    
+    def font_size(self, size:float):
+        self.validate_font()
+        self._font.size = size
+        return self
+    
+    def validate_font(self):
+        if self._font == None:
+            self._font = Font('Arial', 24)
+
+    def render(self, surface):
+        self.validate_font()
+        self._font.render_to(surface, (self.x, self.y), self._text, self._color)
--- src/screens/menu/main_menu.py
+++ src/screens/menu/main_menu.py
@@ -1,9 +1,11 @@
+from typing import Text
 from core.screen import Screen
 from core.window_data import WindowData
 from graphics.widgets.container.container import Container
 from graphics.widgets.container.container_traits import ContainerTraits
 from graphics.widgets.padding.edge_insets import EdgeInsets
 from graphics.widgets.padding.padding import Padding
+from graphics.widgets.text import text
 
 
 class MainMenu(Screen):
@@ -19,24 +21,11 @@ class MainMenu(Screen):
                 color=(212, 212, 212),
                 children=[
                     Container(
-                        width=ContainerTraits.WIDTH_FULL,
-                        height=ContainerTraits.HEIGHT_AUTO,
+                        width=200,
+                        height=50,
                         color=(223, 123, 175),
                         children=[
-                            Padding(
-                                width=ContainerTraits.WIDTH_FULL,
-                                padding=EdgeInsets.symetrical(
-                                    vertical=20,
-                                    horizontal=10
-                                ),
-                                children=[
-                                    Container(
-                                        width=ContainerTraits.WIDTH_FULL,
-                                        height=50,
-                                        color=(122, 12, 10)
-                                    )
-                                ]
-                            )
+                            text.Text('alo')
                         ]
                     )
                 ]
