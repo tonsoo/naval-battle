e812a2c28a4bc99a055dbb135578b42aee0a6e4f
Alysson <alyssuuuuu@outlook.com>
2025-04-24 11:34:59 -0300

    images

---
+++ src/graphics/widgets/image/image.py
@@ -0,0 +1,19 @@
+import pygame
+from graphics.widgets.widget import Widget
+
+
+class Image(Widget):
+    
+    _src:str
+    _image = None
+    
+    def __init__(self, src:str, x=0, y=0, width=0, height=0):
+        super().__init__(x, y, width, height)
+
+        self._src = src
+        self._image = pygame.image.load(src)
+        pygame.transform.scale(self._image, (width, height))
+
+    
+    def render(self, surface):
+        surface.blit(self._image, (self.x, self.y))
