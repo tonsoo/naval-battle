dafa9978f03ac99fd24038a13a2b068dce8b0232
Alysson <alyssuuuuu@outlook.com>
2025-04-24 11:36:27 -0300

    exemplo de imagem

--- src/screens/game.py
+++ src/screens/game.py
@@ -5,6 +5,7 @@ from core.screen import Screen
 from core.window_data import WindowData
 from entities.player import Player
 from graphics.widgets.container.container import Container
+from graphics.widgets.image.image import Image
 
 
 class Game(Screen):
@@ -22,6 +23,9 @@ class Game(Screen):
 
         self._camera.watch(self._player)
         self._camera.addChild(Container(0, 0, 20, 20, Color(255, 0, 0)))
+        self._camera.addChild(
+            Image('assets/icons/close_1.png', x=300, y=122, width=100, height=100)
+        )
         self._camera.addChild(self._player)
         self.addWidget(self._camera)
 
